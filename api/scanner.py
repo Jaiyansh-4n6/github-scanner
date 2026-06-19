c94b1cffeff9a1730d810f53f5053046976c0111
jaiyansh dhaulakhandi <jaiyanshdhaulakhandi@gmail.com>
2026-06-19 17:22:57 +0530

    add scanners\index

---
+++ api/scanner.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+from src.generate_svg import generate_svg
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        username = "Jaiyansh-4n6"
+
+        url = f"https://github.com/users/{username}/contributions"
+
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        cells = soup.select("td[data-date]")
+
+        contribution_data = []
+
+        for cell in cells:
+            contribution_data.append({
+                "date": cell["data-date"],
+                "level": int(cell["data-level"])
+            })
+
+        svg = generate_svg(contribution_data)
+
+        self.send_response(200)
+        self.send_header("Content-Type", "image/svg+xml")
+        self.end_headers()
+
+        self.wfile.write(svg.encode())
