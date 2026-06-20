b392a1b90eb2dbfc0aaefc72cf0e1d3a96ac1001
jaiyansh dhaulakhandi <jaiyanshdhaulakhandi@gmail.com>
2026-06-20 15:48:58 +0530

    fetch live GitHub contributions and contribution count

--- api/index.py
+++ api/index.py
@@ -1,34 +1,54 @@
+from flask import Flask, Response
 import requests
 from bs4 import BeautifulSoup
 from src.generate_svg import generate_svg
+import re
 
+app = Flask(__name__)
 
-def handler(request):
+@app.route("/")
+def scanner():
 
     username = "Jaiyansh-4n6"
 
     url = f"https://github.com/users/{username}/contributions"
 
     response = requests.get(url)
-
     soup = BeautifulSoup(response.text, "html.parser")
 
-    cells = soup.select("td[data-date]")
+    month_labels = []
 
+    for span in soup.select("span[aria-hidden='true']"):
+        text = span.get_text(strip=True)
+        if text in ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]:
+            month_labels.append(text)
+
+    total_contributions = 0
+    h2 = soup.select_one("h2")
+    if h2:
+        text = h2.get_text(strip=True)
+        match = re.search(r"(\d+)", text)
+        if match:
+            total_contributions = int(match.group(1))
+
+    rows = soup.select("tbody tr")
     contribution_data = []
+    for row_index, tr in enumerate(rows):
+        cells = tr.select("td[data-date]")
+        for col_index, cell in enumerate(cells):
+            contribution_data.append({
+                "row": row_index,
+                "col": col_index,
+                "date": cell["data-date"],
+                "level": int(cell["data-level"])})
+    latest_date = max(
+    item["date"]
+    for item in contribution_data
+)
 
-    for cell in cells:
-        contribution_data.append({
-            "date": cell["data-date"],
-            "level": int(cell["data-level"])
-        })
+    svg = generate_svg(contribution_data,total_contributions,latest_date,month_labels)
 
-    svg = generate_svg(contribution_data)
-
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "image/svg+xml"
-        },
-        "body": svg
-    }
+    return Response(
+        svg,
+        mimetype="image/svg+xml"
+    )
